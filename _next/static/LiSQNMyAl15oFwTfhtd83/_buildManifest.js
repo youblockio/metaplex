9b90a226f0158efe2112a16574ee5d3da7cb21dd
cryptoagencyio <cryptoagencyio.dev@gmail.com>
2022-03-03 15:29:07 +0530

    Updates

---
+++ _next/static/LiSQNMyAl15oFwTfhtd83/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-8f35c1bd20692dff863e.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
